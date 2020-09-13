765bd1145d54677c21c31c1b0ef7f609912d8865
Tomek Polański <polanski.tomek@gmail.com>
2020-09-13 09:15:08 +0200

    Investigating timelines (#3)
    Co-authored-by: Tomek Polanski <tpolanski@groupon.com>

--- windows/runner/window_configuration.cpp
+++ windows/runner/window_configuration.cpp
@@ -3,5 +3,5 @@
 const wchar_t* kFlutterWindowTitle = L"healfy";
 const unsigned int kFlutterWindowOriginX = 10;
 const unsigned int kFlutterWindowOriginY = 10;
-const unsigned int kFlutterWindowWidth = 1280;
+const unsigned int kFlutterWindowWidth = 500;
 const unsigned int kFlutterWindowHeight = 720;
