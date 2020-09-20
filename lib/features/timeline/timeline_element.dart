2ff9e5b65cfbabbbb45eedf5d25c9642ac6b20a2
Tomek Polański <polanski.tomek@gmail.com>
2020-09-20 12:48:46 +0200

    Ui improvements (#11)
    * UI improvements
    
    * Adding rive
    
    * Update timeline_element.dart
    
    Co-authored-by: Tomek Polanski <tpolanski@groupon.com>

--- lib/features/timeline/timeline_element.dart
+++ lib/features/timeline/timeline_element.dart
@@ -1,3 +1,4 @@
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 import 'package:healfy/features/timeline/timeline_model.dart';
 
@@ -30,13 +31,10 @@ class TimelineTask extends StatelessWidget {
             ),
             child: Padding(
               padding: const EdgeInsets.all(8),
-              child: Text(
-                data.text,
-                style: Theme.of(context).textTheme.headline6,
-              ),
+              child: TimelineContent(data),
             ),
           ),
-          Container(
+          Material(
             color: color,
             child: Align(
               alignment: Alignment.bottomRight,
@@ -51,6 +49,81 @@ class TimelineTask extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class TimelineContent extends StatelessWidget {
+  const TimelineContent(this.data, {Key key}) : super(key: key);
+
+  final TimelineData data;
+
+  @override
+  Widget build(BuildContext context) {
+    return data.type != TimelineType.wake_up
+        ? Text(
+            data.text,
+            style: Theme.of(context).textTheme.headline6,
+          )
+        : const SizedBox(
+            height: 100,
+            width: 100,
+            child: Rive('alarm', animation: 'play', repeat: 1),
+          );
+  }
+}
+
+class Rive extends StatefulWidget {
+  const Rive(
+    this.asset, {
+    @required this.animation,
+    this.repeat = 0,
+    Key key,
+  }) : super(key: key);
+
+  final String asset;
+  final String animation;
+  final int repeat;
+
+  @override
+  _RiveState createState() => _RiveState();
+}
+
+class _RiveState extends State<Rive> {
+  String _animationName;
+  int repeatCount = 0;
+
+  @override
+  void initState() {
+    _animationName = widget.animation;
+    super.initState();
+  }
+
+  Future<void> _repeat() async {
+    setState(() {
+      _animationName = 'idle';
+    });
+    await Future<void>.delayed(const Duration(seconds: 1));
+    setState(() {
+      _animationName = widget.animation;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        _repeat();
+      },
+      child: FlareActor(
+        'assets/${widget.asset}.flr',
+        animation: _animationName,
+        callback: (_) async {
+          if (repeatCount++ < widget.repeat) {
+            await _repeat();
+          }
+        },
       ),
     );
   }
